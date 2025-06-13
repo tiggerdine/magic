1d940c08e02aaa94e21f16b5fde0be29d99adaf8
Martin Tiggerdine <tiggerdine@gmail.com>
2025-06-13 10:07:47 +0100

    Inject copy button

--- scryfall-enhancer/js/inject.js
+++ scryfall-enhancer/js/inject.js
@@ -1,1 +1,35 @@
-console.debug("Hello, World!");
+const path = window.location.pathname;
+
+const injectCopyButton = () => {
+    const displayOptions = document.querySelector('.search-controls-display-options');
+    const cardsFound = document.querySelector('.search-empty') == null;
+
+    const copyButton = Object.assign(
+        document.createElement('a'), {
+            id: 'copy-button',
+            className: `button-n icon-only${cardsFound ? '' : ' disabled'}`,
+            title: cardsFound ? 'Copy these cards' : 'No cards to copy',
+            href: '#',
+            textContent: '📋'
+        }
+    );
+
+    copyButton.addEventListener('click', copyCards);
+
+    displayOptions.after(copyButton);
+}
+
+const copyCards = () => {
+    const cardGrid = document.querySelector('.card-grid-inner');
+    const cards = Array.from(cardGrid.querySelectorAll(
+        `.card-grid-item:not(.flexbox-spacer)
+         .card-grid-item-card
+         .card-grid-item-invisible-label`
+    )).map(span => span.textContent);
+
+    navigator.clipboard.writeText(cards.join('\n'));
+}
+
+if (path.startsWith('/search')) {
+    injectCopyButton();
+}
